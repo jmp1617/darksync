99901eb66bace642ef89628487c92442ccfc31f6
Jacob Potter <jmp1617@g.rit.edu>
2019-04-11 16:02:38 +0200

    shell still open if nobody is connected

--- darkchat.c
+++ darkchat.c
@@ -423,80 +423,82 @@ void* message_reciever_worker(void* arg){
 void* message_sender_worker(void* arg){
     Metadata meta = (Metadata)arg;
     while(meta->lock!=2){
-        if(meta->ip_count > 1){
+        lock(meta);
+        if(meta->emit_black&&meta->ip_count>1){ // new blacklist item, send it to everyone
+            //grab the most recent addition to the list
+            IP_List temp = meta->ip_list;
+            while(temp->next)
+                temp = temp->next;
+            uint32_t new_black = temp->ip;
+            Message bl_message = calloc(1,sizeof(struct message_s));
+            bl_message->identifier = BL_UPD;
+            bl_message->size = 21; // ident and ip
+            bl_message->message = calloc(20,1);
+            for(int b = 3; b >= 0; b--){ // bytes in decending order
+                bl_message->message[b] |= new_black&0xFF; // set the byte
+                new_black >>= 8; // shift to next
+            }
+            IP_List temp_ip = meta->ip_list->next;
+            printf("Sending new blacklist item...\n");
+            for(int ip=1; ip < meta->ip_count; ip++){
+                // connect to node
+                struct sockaddr_in node;
+                node.sin_family = AF_INET;
+                node.sin_port = htons(RPORT);
+                node.sin_addr.s_addr = temp_ip->ip;
+                while(connect(meta->sender_s, (struct sockaddr *)&node, sizeof(node)) < 0);
+                send_message(bl_message, meta->sender_s);
+                close(meta->sender_s);
+                meta->sender_s = init_socket(SPORT);
+                temp_ip=temp_ip->next;
+            }
+            free(bl_message->message);
+            free(bl_message);
+        }
+        unlock(meta);
+        printf("> ");
+        char message[MAXMSGLEN] = {0};
+        fgets(message,MAXMSGLEN,stdin);
+        if(message[0]=='/'&&message[1]=='q'&&message[2]=='\n'){
+            printf("Quitting...\n");
+            // send disconnect
             lock(meta);
-            if(meta->emit_black){ // new blacklist item, send it to everyone
-                //grab the most recent addition to the list
-                IP_List temp = meta->ip_list;
-                while(temp->next)
-                    temp = temp->next;
-                uint32_t new_black = temp->ip;
-                Message bl_message = calloc(1,sizeof(struct message_s));
-                bl_message->identifier = BL_UPD;
-                bl_message->size = 21; // ident and ip
-                bl_message->message = calloc(20,1);
-                for(int b = 3; b >= 0; b--){ // bytes in decending order
-                    bl_message->message[b] |= new_black&0xFF; // set the byte
-                    new_black >>= 8; // shift to next
-                }
+            if(meta->ip_count > 1){
+                Message disconnect = calloc(1,sizeof(struct message_s));
+                disconnect->identifier = DISCONNECT;
+                disconnect->size = 1;
                 IP_List temp_ip = meta->ip_list->next;
-                printf("Sending new blacklist item...\n");
-                for(int ip=1; ip < meta->ip_count; ip++){
-                    // connect to node
+                for(int ip = 1; ip < meta->ip_count; ip++){
                     struct sockaddr_in node;
                     node.sin_family = AF_INET;
                     node.sin_port = htons(RPORT);
                     node.sin_addr.s_addr = temp_ip->ip;
-                    while(connect(meta->sender_s, (struct sockaddr *)&node, sizeof(node)) < 0);
-                    send_message(bl_message, meta->sender_s);
+                    while(connect(meta->sender_s, (struct sockaddr *)&node,sizeof(node)) < 0);
+                    send_message(disconnect, meta->sender_s);
                     close(meta->sender_s);
                     meta->sender_s = init_socket(SPORT);
                     temp_ip=temp_ip->next;
                 }
-                free(bl_message->message);
-                free(bl_message);
+                free(disconnect);
             }
             unlock(meta);
-            printf("> ");
-            char message[MAXMSGLEN] = {0};
-            fgets(message,MAXMSGLEN,stdin);
-            if(message[0]=='/'&&message[1]=='q'&&message[2]=='\n'){
-                printf("Quitting...\n");
-                // send disconnect
-                if(meta->ip_count > 1){
-                    Message disconnect = calloc(1,sizeof(struct message_s));
-                    disconnect->identifier = DISCONNECT;
-                    disconnect->size = 1;
-                    lock(meta);
-                    IP_List temp_ip = meta->ip_list->next;
-                    for(int ip = 1; ip < meta->ip_count; ip++){
-                        struct sockaddr_in node;
-                        node.sin_family = AF_INET;
-                        node.sin_port = htons(RPORT);
-                        node.sin_addr.s_addr = temp_ip->ip;
-                        while(connect(meta->sender_s, (struct sockaddr *)&node,sizeof(node)) < 0);
-                        send_message(disconnect, meta->sender_s);
-                        close(meta->sender_s);
-                        meta->sender_s = init_socket(SPORT);
-                        temp_ip=temp_ip->next;
-                    }
-                    unlock(meta);
-                    free(disconnect);
-                }
-                meta->lock = 2;
-                shutdown(meta->reciever_s,SHUT_RDWR);
-            }
-            else if(message[0]=='/'&&message[1]=='l'&&message[2]=='\n'){
-                lock(meta);
-                printf("Connected (%d)\n",meta->ip_count);
-                IPL_print(meta->ip_list);
-                printf("Blacklist (%d)\n",meta->blacklist_count);
-                IPL_print(meta->blacklist);
-                unlock(meta);
-            }
-            else{ // normal message
+            meta->lock = 2;
+            shutdown(meta->reciever_s,SHUT_RDWR);
+        }
+        else if(message[0]=='/'&&message[1]=='l'&&message[2]=='\n'){
+            lock(meta);
+            printf("Connected (%d)\n",meta->ip_count);
+            IPL_print(meta->ip_list);
+            printf("Blacklist (%d)\n",meta->blacklist_count);
+            IPL_print(meta->blacklist);
+            unlock(meta);
+        }
+        else{ // normal message
+            lock(meta);
+            if(meta->ip_count > 1){
 
             }
+            unlock(meta);
         }
     }
     return 0;
